7795d5f4afdbc5f2bf765d62f2895e51b31e4c81
Yury Senna <yurysenasilva@gmail.com>
2025-06-22 20:36:53 -0300

    .

--- script.js
+++ script.js
@@ -5,3 +5,32 @@ function mudarTema() {
     corpo.classList.toggle('light');
     tema.textContent = corpo.classList.contains('light') ? "☀️" : "🌙"; 
 }
+
+async function carregarDadosClima() {
+    const apiKey = "a578fa374c35314a5e3ef055159a72f4";
+    const cidade = "Sao Paulo";
+    const pais = "BR";
+
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${cidade},${pais}&appid=${apiKey}&units=metric&lang=pt_br`;
+
+    try {
+      const response = await fetch(url);
+      const dados = await response.json();
+
+      const temperatura = Math.round(dados.main.temp);
+      const descricao = dados.weather[0].description;
+
+      const agora = new Date();
+      const dia = agora.toLocaleDateString("pt-BR");
+      const hora = agora.toLocaleTimeString("pt-BR", { hour: '2-digit', minute: '2-digit' });
+
+      document.getElementById("tempo-agora").innerHTML =
+        `📍 ${cidade} | ${descricao} | 🌡️ ${temperatura}°C | 🕒 ${dia} - ${hora}`;
+
+    } catch (error) {
+      console.error("Erro ao carregar clima:", error);
+      document.getElementById("weather-info").textContent = "Erro ao carregar informações.";
+    }
+  }
+
+  carregarDadosClima();
